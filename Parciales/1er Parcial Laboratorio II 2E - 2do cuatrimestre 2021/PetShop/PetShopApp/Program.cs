3a532ce822b52d4388352c1fb435be1eace7ce8a
“nicolassistema” <nicolas.letticugna@gmail.com>
2021-09-28 22:20:42 -0300

    Subiendo segundo form

--- Parciales/1er Parcial Laboratorio II 2E - 2do cuatrimestre 2021/PetShop/PetShopApp/Program.cs
+++ Parciales/1er Parcial Laboratorio II 2E - 2do cuatrimestre 2021/PetShop/PetShopApp/Program.cs
@@ -18,6 +18,7 @@ namespace PetShopApp
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Login());
+            //Application.Run(new Administracion());
         }
     }
 }
